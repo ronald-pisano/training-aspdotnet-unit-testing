74ea93d600ff6b0feb935efd6106fd31c009634c
Ronald Pisano <ronald.pisano@conduitassociates.com>
2024-12-29 11:41:56 +0800

    add solution to basic exercise

--- LearningMaterial/CalculatorLibrary.Tests.Unit/CalculatorTests.cs
+++ LearningMaterial/CalculatorLibrary.Tests.Unit/CalculatorTests.cs
@@ -7,9 +7,9 @@ public class CalculatorTests
     private readonly Calculator _sut = new();
 
     [Theory]
-    [InlineData(5, 4, 9)]
-    [InlineData(0, 0, 0)]
-    [InlineData(-5, -5, -10)]
+    [InlineData(5, 5, 10)]
+    [InlineData(-5, 5, 0)]
+    [InlineData(-15, -5, -20)]
     public void Add_ShouldAddTwoNumbers_WhenTwoNumbersAreIntegers(
         int a, int b, int expected)
     {
@@ -20,13 +20,44 @@ public class CalculatorTests
         Assert.Equal(expected, result);
     }
 
-    [Fact]
-    public void Subtract_ShouldSubtractTwoNumbers_WhenTwoNumbersAreIntegers()
+    [Theory]
+    [InlineData(5, 5, 0)]
+    [InlineData(15, 5, 10)]
+    [InlineData(-5, -5, 0)]
+    [InlineData(-15, -5, -10)]
+    [InlineData(-5, 10, -15)]
+    public void Subtract_ShouldSubtractTwoNumbers_WhenTwoNumbersAreIntegers(int a, int b, int expected)
     {
         // Act
-        var result = _sut.Subtract(10, 5);
+        var result = _sut.Subtract(a, b);
 
         // Assert
-        Assert.Equal(5, result);
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(5, 5, 25)]
+    [InlineData(50, 0, 0)]
+    [InlineData(-5, 5, -25)]
+    public void Multiply_ShouldMultiplyTwoNumbers_WhenTwoNumbersAreIntegers(int a, int b, int expected)
+    {
+        // Act
+        var result = _sut.Multiply(a, b);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(5, 5, 1)]
+    [InlineData(15, 5, 3)]
+    [InlineData(5, 0, 0, Skip = "Division by zero is mathematically impossible")]
+    public void Divide_ShouldDivideTwoNumbers_TwoNumbersAreIntegers(int a, int b, int expected)
+    {
+        // Act
+        var result = _sut.Divide(a, b);
+
+        // Assert
+        Assert.Equal(expected, result);
     }
 }
